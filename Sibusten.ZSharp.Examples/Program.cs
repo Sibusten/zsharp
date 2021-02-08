bb8a59be6a51304ef46bd5565f07073967c8c5e1
Sibusten <Sibusten@gmail.com>
2021-02-08 18:52:06 -0500

    Initial Commit

---
+++ Sibusten.ZSharp.Examples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sibusten.ZSharp.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
